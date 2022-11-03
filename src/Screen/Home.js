719655d1752951096cd4d3c6924bdd3f295645d9
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2022-11-03 21:29:35 +0600

    ok

--- src/Screen/Home.js
+++ src/Screen/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../Styles/Home.css';
 import {
   Button, Checkbox, Form, Input, Layout,
@@ -9,6 +9,7 @@ import { HomeOutlined } from '@ant-design/icons';
 const { Header, Footer, Sider, Content } = Layout;
 
 export default function Home() {
+  const [page, setPage] = useState(1)
 
   return (
     <div className='HomeHeaderBody'>
@@ -18,33 +19,57 @@ export default function Home() {
         </div>
         <div className='HomeLine' />
         <div className='HomeLeftDownDiv'>
-          <div className='HomeLeftDownButtonDiv'>
-            <HomeOutlined style={{ color: 'blue', fontSize: '20px', marginTop: '-20px', flex: '2' }} />
-            <p style={{ color: 'blue', fontSize: '20px', flex: '2' }}>Home</p>
-            <div style={{ flex: '4' }}>
-            </div>
-            <div className='HomeLeftLineDiv'></div>
-          </div>
-
-          <div className='HomeLeftDownButtonDiv'>
-            <HomeOutlined style={{ color: 'blue', fontSize: '20px', marginTop: '-20px', flex: '2' }} />
-            <p style={{ color: 'blue', fontSize: '20px', flex: '2' }}>Home</p>
-            <div style={{ flex: '4' }}>
-            </div>
-            <div className='HomeLeftLineDiv'></div>
-          </div>
-          <Button type="text" className='HomeLeftDownButton'>
-            <HomeOutlined style={{ color: 'blue', fontSize: '20px', marginTop: '-20px',marginLeft:'10px' }} />
-            <p style={{ color: 'blue', fontSize: '20px', marginLeft:'30px' }}>Home</p>
-            
-            <div className='HomeLeftLineDiv'></div>
+          <Button type="text" onClick={() => {
+            setPage(1)
+          }} className='HomeLeftDownButton'>
+            <HomeOutlined style={{ color: page == 1 ? 'blue' : 'rgb(152, 152, 158)', fontSize: '20px', marginTop: '-20px', marginLeft: '10px' }} />
+            <p style={{ color: page == 1 ? 'black' : 'rgb(152, 152, 158)', fontSize: '20px', marginLeft: '30px' }}>Home</p>
+            {page==1 ?(
+              <div className='HomeLeftLineDiv'></div>
+            ):(<></>)}
           </Button>
-          <div>ddd</div>
+
+          <Button type="text" onClick={() => {
+            setPage(2)
+          }} className='HomeLeftDownButton'>
+            <HomeOutlined style={{ color: page == 2 ? 'blue' : 'rgb(152, 152, 158)', fontSize: '20px', marginTop: '-20px', marginLeft: '10px' }} />
+            <p style={{ color: page == 2 ? 'black' : 'rgb(152, 152, 158)', fontSize: '20px', marginLeft: '30px' }}>Home</p>
+            {page==2 ?(
+              <div className='HomeLeftLineDiv'></div>
+            ):(<></>)}
+          </Button>
+
+          <Button type="text" onClick={() => {
+            setPage(3)
+          }} className='HomeLeftDownButton'>
+            <HomeOutlined style={{ color: page == 3 ? 'blue' : 'rgb(152, 152, 158)', fontSize: '20px', marginTop: '-20px', marginLeft: '10px' }} />
+            <p style={{ color: page == 3 ? 'black' : 'rgb(152, 152, 158)', fontSize: '20px', marginLeft: '30px' }}>Home</p>
+            
+            {page==3 ?(
+              <div className='HomeLeftLineDiv'></div>
+            ):(<></>)}
+          </Button>
         </div>
       </div>
-      <div className='HomeHeaderBodyDiv2'>
-        <div>Main Dashbord</div>
+      <div style={{ height: 'auto', width: '73%', }}>
+        <div className='HomeHeaderBodyDiv2'>
+          <div>Main Dashbord</div>
+        </div>
+        <div>
+          {
+            page == 1 ? (
+              <div> akash 1</div>
+            ) : page == 2 ? (
+              <div> akash 2</div>
+            ) : page == 3 ? (
+              <div> akash 3</div>
+            ) : (
+              <p>Page Error</p>
+            )
+          }
+        </div>
       </div>
+
     </div>
   )
 }
