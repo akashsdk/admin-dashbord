9d0430ffffd5b7e4953aece13e4f03dc25a298f9
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2022-11-03 20:38:08 +0600

    ok

--- src/Screen/Home.js
+++ src/Screen/Home.js
@@ -4,6 +4,7 @@ import {
   Button, Checkbox, Form, Input, Layout,
   Col, Row, Radio
 } from 'antd';
+import { HomeOutlined } from '@ant-design/icons';
 
 const { Header, Footer, Sider, Content } = Layout;
 
@@ -18,8 +19,26 @@ export default function Home() {
         <div className='HomeLine' />
         <div className='HomeLeftDownDiv'>
           <div className='HomeLeftDownButtonDiv'>
-
+            <HomeOutlined style={{ color: 'blue', fontSize: '20px', marginTop: '-20px', flex: '2' }} />
+            <p style={{ color: 'blue', fontSize: '20px', flex: '2' }}>Home</p>
+            <div style={{ flex: '4' }}>
+            </div>
+            <div className='HomeLeftLineDiv'></div>
           </div>
+
+          <div className='HomeLeftDownButtonDiv'>
+            <HomeOutlined style={{ color: 'blue', fontSize: '20px', marginTop: '-20px', flex: '2' }} />
+            <p style={{ color: 'blue', fontSize: '20px', flex: '2' }}>Home</p>
+            <div style={{ flex: '4' }}>
+            </div>
+            <div className='HomeLeftLineDiv'></div>
+          </div>
+          <Button type="text" className='HomeLeftDownButton'>
+            <HomeOutlined style={{ color: 'blue', fontSize: '20px', marginTop: '-20px',marginLeft:'10px' }} />
+            <p style={{ color: 'blue', fontSize: '20px', marginLeft:'30px' }}>Home</p>
+            
+            <div className='HomeLeftLineDiv'></div>
+          </Button>
           <div>ddd</div>
         </div>
       </div>
