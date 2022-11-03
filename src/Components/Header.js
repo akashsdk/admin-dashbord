39beeba7859c47ada3240b89cf7f75467c7521fc
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2022-11-03 19:55:12 +0600

    ok

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,13 +3,6 @@ import './Header.css';
 
 export default function Header() {
   return (
-    <div className='HeaderBody'>
-        <div className='HeaderBodyDiv'>
-
-        </div>
-        <div className='HeaderBodyDiv2'>
-            <div>Main Dashbord</div>
-        </div>
-    </div>
+    <div></div>
   )
 }
--- src/Screen/Home.js
+++ src/Screen/Home.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import '../Styles/Home.css';
+import {
+  Button, Checkbox, Form, Input, Layout,
+  Col, Row, Radio
+} from 'antd';
+
+const { Header, Footer, Sider, Content } = Layout;
 
 export default function Home() {
+
   return (
-    <div>Home</div>
+    <div className='HomeHeaderBody'>
+      <div className='HomeHeaderBodyDiv'>
+        <div className='HomeLeftDiv'>
+          <h1 style={{ marginTop: '30px' }}>Admin Dashbord</h1>
+        </div>
+        <div className='HomeLine' />
+        <div className='HomeLeftDownDiv'>
+          <div className='HomeLeftDownButtonDiv'>
+
+          </div>
+          <div>ddd</div>
+        </div>
+      </div>
+      <div className='HomeHeaderBodyDiv2'>
+        <div>Main Dashbord</div>
+      </div>
+    </div>
   )
 }
