ce814b561033fbfa9bcb8f98871ce45e55af5742
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2022-11-04 01:10:01 +0600

    ok

--- src/Screen/Home.js
+++ src/Screen/Home.js
@@ -2,17 +2,44 @@ import React, { useState } from 'react';
 import '../Styles/Home.css';
 import {
   Button, Checkbox, Form, Input, Layout,
-  Col, Row, Radio, message,
+  Col, Row, Radio, message, notification,
+  Drawer, Space, Modal,
 } from 'antd';
-import { HomeOutlined, SearchOutlined,BellOutlined } from '@ant-design/icons';
+import { HomeOutlined, SearchOutlined, BellOutlined, MenuOutlined } from '@ant-design/icons';
 
 const { Header, Footer, Sider, Content } = Layout;
 const key = 'updatable';
+const openNotification = () => {
+  const args = {
+    message: 'Notification Title',
+    description:
+      'I will never close automatically. This is a purposely very very long description that has many many characters and words.',
+    duration: 0,
+  };
+  notification.open(args);
+};
 
 
 export default function Home() {
   const [page, setPage] = useState(1)
- 
+  const [open, setOpen] = useState(false);
+  const showDrawer = () => {
+    setOpen(true);
+  };
+  const onClose = () => {
+    setOpen(false);
+  };
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
 
   return (
     <div className='HomeHeaderBody'>
@@ -87,9 +114,9 @@ export default function Home() {
           </Button>
         </div>
       </div>
-      <div style={{ height: 'auto', width: '73%', }}>
+      <div style={{ height: 'auto', width: '100%', }}>
         <div className='HomeHeaderBodyDiv2'>
-          <div style={{ flex: '1' }}>
+          <div style={{ flex: '1.5' }}>
             {
               page == 1 ? (
                 <h3 style={{ opacity: '.6', marginTop: '10px' }}>Page / Home 1</h3>
@@ -104,10 +131,96 @@ export default function Home() {
 
             <h1 style={{ marginTop: '-15px' }}>Dashbord</h1>
           </div>
-          <div style={{ flex: '2' }} />
+          <div style={{ flex: '1.5' }} />
           <div className='HomeSearchDiv'>
             <Input placeholder="Search" className='HomeSearchInput' prefix={<SearchOutlined />} />
-            <BellOutlined className='HomeSearchNotification' />
+            <MenuOutlined onClick={showDrawer} className='HomeSearchMenu' />
+            <BellOutlined onClick={openNotification} className='HomeSearchNotification' />
+            <Drawer
+              title=""
+              placement='left'
+              width={300}
+              onClose={onClose}
+              open={open}
+              extra={
+                <Space>
+                  <Button onClick={onClose}>Cancel</Button>
+                  <Button type="primary" onClick={onClose}>
+                    OK
+                  </Button>
+                </Space>
+              }
+            >
+              <div className='HomeLeftDownDiv'>
+                <Button type="text" onClick={() => {
+                  setPage(1);
+                  message.loading({
+                    content: 'Loading...',
+                    key,
+                  });
+                  setTimeout(() => {
+                    message.success({
+                      content: 'Home Page',
+                      key,
+                      duration: 2,
+                    });
+                  }, 1000);
+                }} className='HomeLeftDownButton'>
+                  <HomeOutlined style={{ color: page == 1 ? 'blue' : 'rgb(152, 152, 158)', fontSize: '20px', marginTop: '-20px', marginLeft: '10px' }} />
+                  <p style={{ color: page == 1 ? 'black' : 'rgb(152, 152, 158)', fontSize: '20px', marginLeft: '30px' }}>Home</p>
+                  {page == 1 ? (
+                    <div className='HomeLeftLineDiv'></div>
+                  ) : (<></>)}
+                </Button>
+
+                <Button type="text" onClick={() => {
+                  setPage(2);
+                  message.loading({
+                    content: 'Loading...',
+                    key,
+                  });
+                  setTimeout(() => {
+                    message.success({
+                      content: 'Home Page 2',
+                      key,
+                      duration: 2,
+                    });
+                  }, 1000);
+                }} className='HomeLeftDownButton'>
+                  <HomeOutlined style={{ color: page == 2 ? 'blue' : 'rgb(152, 152, 158)', fontSize: '20px', marginTop: '-20px', marginLeft: '10px' }} />
+                  <p style={{ color: page == 2 ? 'black' : 'rgb(152, 152, 158)', fontSize: '20px', marginLeft: '30px' }}>Home</p>
+                  {page == 2 ? (
+                    <div className='HomeLeftLineDiv'></div>
+                  ) : (<></>)}
+                </Button>
+
+                <Button type="text" onClick={() => {
+                  setPage(3);
+                  message.loading({
+                    content: 'Loading...',
+                    key,
+                  });
+                  setTimeout(() => {
+                    message.success({
+                      content: 'Home Page',
+                      key,
+                      duration: 2,
+                    });
+                  }, 1000);
+                }} className='HomeLeftDownButton'>
+                  <HomeOutlined style={{ color: page == 3 ? 'blue' : 'rgb(152, 152, 158)', fontSize: '20px', marginTop: '-20px', marginLeft: '10px' }} />
+                  <p style={{ color: page == 3 ? 'black' : 'rgb(152, 152, 158)', fontSize: '20px', marginLeft: '30px' }}>Home</p>
+
+                  {page == 3 ? (
+                    <div className='HomeLeftLineDiv'></div>
+                  ) : (<></>)}
+                </Button>
+              </div>
+            </Drawer>
+            <div onClick={showModal} className='HomeSearchProfile'></div>
+            <Modal title="👋  Hey, Akash" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+              
+            </Modal>
           </div>
           <div style={{ width: '10px' }} />
         </div>
@@ -118,7 +231,9 @@ export default function Home() {
             ) : page == 2 ? (
               <div> akash 2</div>
             ) : page == 3 ? (
-              <div> akash 3</div>
+              <div>
+                akash 3
+              </div>
             ) : (
               <p>Page Error</p>
             )
