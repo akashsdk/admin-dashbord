c870d0283a3eaf816a90907de977e5ad63425995
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2022-11-04 18:38:33 +0600

    ok

--- src/Card/SmallCard.js
+++ src/Card/SmallCard.js
@@ -2,15 +2,15 @@ import React from 'react';
 import './Styles/SmallCard.css';
 import { BarChartOutlined} from '@ant-design/icons';
 
-export default function SmallCard() {
+export default function SmallCard(props) {
     return (
         <div className='SmallCardBody'>
             <div className='SmallCardLeft'>
                 <BarChartOutlined className='SmallCardLeftIcon'/>
             </div>
             <div className='SmallCardRight'>
-                <p className='SmallCardRightP'>Earnings</p>
-                <p className='SmallCardRightP2'>$350.4</p>
+                <p className='SmallCardRightP'> {props.text}</p>
+                <p className='SmallCardRightP2'>${props.amount}</p>
             </div>
         </div>
     )
--- src/IndexPage/MainDashboard.js
+++ src/IndexPage/MainDashboard.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import './Styles/MainDashboard.css';
 import SmallCard from '../Card/SmallCard';
+import { BarChartOutlined} from '@ant-design/icons';
+import { FaDollarSign } from "react-icons/fa";
 
 export default function MainDashboard() {
     return (
         <div className='MainDashBody'>
             <div className='MainDashBodyDiv'>
-                <SmallCard />
-                <SmallCard />
-                <SmallCard />
+                <SmallCard text='Earnings' amount='350.4'/>
+                <SmallCard text='Spend this month' amount='1,500'/>
+                <SmallCard text='Sales' amount='580.56'/>
             </div>
             <div className='MainDashBodyDiv'>
                 <SmallCard />
                 <SmallCard />
                 <SmallCard />
             </div>
+            <FaDollarSign />
         </div>
     )
 }
