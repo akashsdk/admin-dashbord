6423e27db49d7c68bb5f623853011f3a0bb57900
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2022-11-08 20:41:29 +0600

    ok

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './Header.css';
+import { PageHeader } from 'antd';
+import { Link, } from "react-router-dom";
 
 export default function Header() {
   return (
     <div>
-      <div>frd</div>
+      <PageHeader
+      
+        className="site-page-header"
+        onBack={() =>{
+          
+        }}
+        title="UK HR Cloud"
+        subTitle=""
+      />
     </div>
   )
 }
