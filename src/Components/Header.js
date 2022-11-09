ef5dd3fa2f2e1d7a71d29effb5067780b8918c6d
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2022-11-10 01:54:46 +0600

    ok

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,12 +2,21 @@ import React from 'react';
 import './Header.css';
 import { BackTop, PageHeader } from 'antd';
 import { Link, } from "react-router-dom";
+import {
+  HomeOutlined,
+} from '@ant-design/icons';
+
+
 
 export default function Header() {
   return (
     <div className='Header-Body'>
-      <div>akash</div>
-      <div>aaa</div>
+      <div style={{flex:'1'}}>
+        <Link>
+      <HomeOutlined />
+        </Link>
+      </div>
+      <div style={{flex:'5'}}>aaa</div>
       {/* <PageHeader
       
         className="site-page-header"
